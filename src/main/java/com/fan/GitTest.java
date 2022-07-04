b9f49242647916c3e70a4da747a72588c2915654
fan <>
2022-07-04 14:13:24 +0800

    fifth commit

--- src/main/java/com/fan/GitTest.java
+++ src/main/java/com/fan/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("third commit");
         System.out.println("hot-fix commit");
         System.out.println("fourth commit");
+        System.out.println("fifth commit");
     }
 }

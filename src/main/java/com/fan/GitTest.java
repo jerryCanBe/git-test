c7139e6589671988dfb4bc08722801dabf374622
fan <>
2022-07-04 13:40:53 +0800

    second commit

--- src/main/java/com/fan/GitTest.java
+++ src/main/java/com/fan/GitTest.java
@@ -3,6 +3,7 @@ package com.fan;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("hello jerry");
+        System.out.println("hello fan");
     }
 }
